f2cc8a83646cb024552169ba6cfad1ebb84a8684
hetpatel4381 <hetpatel2808hp@gmail.com>
2023-06-27 13:15:09 +0530

    error in vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Modal from "./components/modal/modal";
+import { preview } from "vite";
 function App() {
   const [item, setItem] = React.useState([
     { id: "1", name: "abce", description: "random text1" },
@@ -10,45 +11,63 @@ function App() {
     { id: "5", name: "ijk", description: "random text5" },
   ]);
   const [showModal, setShowModal] = React.useState(false);
+  const [newItem, setNewItem] = useState({ id: "", name: "", description: ""});
+  
   function handleRemove(itemID) {
     const newlist = item.filter((item) => item.id !== itemID);
     setItem(newlist);
+  }
+
+  function handleAddItem(){
+    setItem((prevItems) => [...prevItems, newItem]);
+    setShowModal(false);
+    setNewItem({id: "", name: "", description: ""});
   }
   
   return (
     <div className="app">
       <div className="app-wrapper">
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {item.map((item, index) => (
-            <tr key={index}>
-              <td>{item.id}</td>
-              <td>{item.name}</td>
-              <td>{item.description}</td>
-              <td>
-                <button
-                  onClick={() => handleRemove(item.id)}
-                  className="td-btn"
-                >
-                  Remove
-                </button>
-              </td>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Action</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-        <button className="add-btn" onClick={() => setShowModal(prev => !prev)}>Add</button>
-        {showModal ? <Modal setShowModal={setShowModal}/> : ""}
-        
-        </div>
+          </thead>
+          <tbody>
+            {item.map((item, index) => (
+              <tr key={index}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.description}</td>
+                <td>
+                  <button
+                    onClick={() => handleRemove(item.id)}
+                    className="td-btn"
+                  >
+                    Remove
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <button
+          className="add-btn"
+          onClick={() => setShowModal(true)}
+        >
+          Add
+        </button>
+        {showModal && (
+          <Modal
+            setShowModal={setShowModal}
+            setNewItem={setNewItem}
+            handleAddItem={handleAddItem}
+          />
+        )}
+      </div>
     </div>
   );
 }
--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./modal.css";
-const Modal = ({setShowModal}) => {
+const Modal = ({setShowModal, setNewItem, handleAddItem}) => {
+
+  const [id, setId] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+
     function handleCloseModal(){
         setShowModal(false);
     }
+
+    function handleFormSubmit(event){
+      event.preventDefault();
+      const newItem = {id, name, description};
+      setNewItem(newItem);
+      handleAddItem();
+    }
+
   return (
     <div className="modal">
       <div className="modal wrapper ">
-        <h1>Modal</h1>
-        
-        <div className="modal-input-wrapper">
-          <input
-            placeholder="ID"
-            className="modal-input"
-            aria-label="ID"
-          ></input>
-          <input placeholder="Name" className="modal-input"></input>
-          <input placeholder="Description" className="modal-input"></input>
-        </div>
-        <div className="modal-btn-wrapper">
-          <button className="modal-btn">Add</button>
-        </div>
+        <h1>Add Information</h1>
+        <form onSubmit={handleFormSubmit}>
+          <div className="modal-input-wrapper">
+            <input
+              placeholder="ID"
+              className="modal-input"
+              aria-label="ID"
+              value={id}
+              onChange={(e) => setId(e.target.value)}
+            ></input>
+            <input placeholder="Name" className="modal-input"
+            value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+            <input placeholder="Description" className="modal-input"
+            value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            ></input>
+          </div>
+          <div className="modal-btn-wrapper">
+            <button type="submit" className="modal-btn">Submit</button>
+          </div>
+        </form>
       </div>
     </div>
   );
